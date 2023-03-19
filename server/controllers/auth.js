8832f05716ec31db32f250ecf5e80d837b8c3fe5
Kene <kayyungx@gmail.com>
2023-03-19 14:37:39 +0100

    Add login authentication

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -44,3 +44,41 @@ export const register = async (req, res) => {
     });
   }
 };
+
+//* Login User
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({
+        status: "fail",
+        message: "Wrong email or password",
+      });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({
+        status: "fail",
+        message: "Invalid credentials",
+      });
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    delete user.password;
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        user,
+        token,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      message: error.message,
+    });
+  }
+};
