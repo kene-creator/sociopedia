48ba326bf6c6d6370d328a0d33edd9c8b583b99a
Kene <kayyungx@gmail.com>
2023-03-21 10:13:23 +0100

    setup store and actions with redux

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import authReducer from "./state/state.js";
+import Provider from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  PERSIST,
+  REHYDRATE,
+  PURSE,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import persistGate from "redux-persist/integration/react";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
---
+++ client/src/state/state.js
@@ -0,0 +1,50 @@
+import { createSlice } from "redux-toolkit";
+
+const initialState = {
+  mode: "light",
+  user: null,
+  token: null,
+  post: [],
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setMode: (state) => {
+      state.mode = state.mode === "light" ? "dark" : "light";
+    },
+    setLogin: (state, action) => {
+      state.user = action.payload.user;
+      state.token = action.payload.token;
+    },
+    setLogout: (state) => {
+      state.user = null;
+      state.token = null;
+    },
+    setFriends: (state, action) => {
+      if (state.user) {
+        state.user.friends = action.payload.friends;
+      } else {
+        console.log("User freind not found");
+      }
+    },
+    setPosts: (state, action) => {
+      state.post = action.payload.post;
+    },
+    setPost: (state, action) => {
+      const updatedPost = state.post.map((post) => {
+        if (post._id === action.payload.post._id) {
+          return action.payload.post;
+        } else {
+          return post;
+        }
+      });
+      state.post = updatedPost;
+    },
+  },
+});
+
+export const { setMode, setLogin, setLogout, setFriends, setPosts, setPost } =
+  authSlice.actions;
+export default authSlice.reducer;
