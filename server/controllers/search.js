12c329c0837ef723aff7f08c3047737a86e9b615
Kene <kayyungx@gmail.com>
2023-03-24 08:26:24 +0100

    Implement search and update user schema

---
+++ server/controllers/search.js
@@ -0,0 +1,18 @@
+import User from "../models/User.js";
+
+const search = async (req, res) => {
+  try {
+    let payload = req.body.payload.trim();
+    let users = await User.find({
+      firstName: { $regex: payload, $options: "i" },
+    });
+    console.log(users);
+
+    res.status(200).json({ users });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ error });
+  }
+};
+
+export default search;
