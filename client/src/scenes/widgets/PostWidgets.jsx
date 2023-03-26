76ec1ca3cd9f0b3f77103881cfc7159b60597fa4
Kene <kayyungx@gmail.com>
2023-03-26 15:55:43 +0100

    Add friend component

--- client/src/scenes/widgets/PostWidgets.jsx
+++ client/src/scenes/widgets/PostWidgets.jsx
@@ -1,4 +1,13 @@
 import React from "react";
+import {
+  ChatBubbleOutlineOutlined,
+  FavoriteBorderOutlined,
+  FavoriteOutlined,
+  ShareOutlined,
+} from "@mui/icons-material";
+import { Box, Divider, Typography, useTheme, IconButton } from "@mui/material";
+import FlexBetween from "../../components/FlexBetween";
+import Friends from "../../components/Friends";
 
 export default function PostWidgets() {
   return <div>PostWidgets</div>;
